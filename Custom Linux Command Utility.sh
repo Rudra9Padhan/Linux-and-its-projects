e16a3ee26165fa7b327a267ff8cc174392011d03
Rudranarayan Padhan <161827362+Rudra9Padhan@users.noreply.github.com>
2025-09-13 22:45:15 +0530

    Update Custom Linux Command Utility.sh

--- Custom Linux Command Utility.sh
+++ Custom Linux Command Utility.sh
@@ -5,3 +5,22 @@ echo "Listing files in $(pwd):"
 for file in *; do
     echo "$file"
 done
+
+-------------------------------
+#!/bin/bash
+
+# Colors
+DIR="\e[34m"   # Blue for directories
+EXE="\e[32m"   # Green for executables
+REG="\e[0m"    # Reset color
+
+echo "Custom ls output from: $(pwd)"
+for file in *; do
+    if [ -d "$file" ]; then
+        echo -e "${DIR}$file${REG}"
+    elif [ -x "$file" ]; then
+        echo -e "${EXE}$file${REG}"
+    else
+        echo "$file"
+    fi
+done
